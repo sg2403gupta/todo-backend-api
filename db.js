dcc0699b06d1c428b40d11490587f226cf83c002
Shubham Gupta <guptashubahm0324@gmail.com>
2025-11-30 18:04:34 +0530

    Added API routes with zod validation

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ Connected to MongoDB"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.ObjectId;
 
 const User = new Schema({
-  name: { type: String, unique: true, required: true },
-  email: String,
+  name: String,
+  email: { type: String, unique: true },
   password: String,
 });
 
--- index.js
+++ index.js
@@ -2,38 +2,98 @@ require("dotenv").config();
 const { TodoModel, UserModel } = require("./db");
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const z = require("zod");
 const JWT_SECRET = process.env.JWT_SECRET;
 const app = express();
 app.use(express.json());
 
 app.post("/signup", async function (req, res) {
+  //Input validation using zod
+  const rqstBody = z.object({
+    name: z
+      .string()
+      .trim()
+      .min(3, "Name must be atleat 3 characters long")
+      .max(30, "Name too long"),
+    email: z
+      .string()
+      .trim()
+      .min(3)
+      .max(50)
+      .email("Invalid email format")
+      .regex(
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Email must be valid and contain a proper domain"
+      ),
+    password: z
+      .string()
+      .min(8, "Password must contain atleast 8 characters")
+      .max(64, "Password too long")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(
+        /[@$!%*?&]/,
+        "Password must contain at least one special character (@, $, !, %, *, ?, &)"
+      ),
+  });
+
+  const passedWithSuccess = rqstBody.safeParse(req.body);
+
+  if (!passedWithSuccess.success) {
+    res.json({
+      message: "Invalid Credentials",
+    });
+    return;
+  }
+
   const name = req.body.name;
   const password = req.body.password;
   const email = req.body.email;
 
-  await UserModel.create({
-    name: name,
-    email: email,
-    password: password,
-  });
+  let errorThrown = false;
 
-  res.json({
-    message: "You successfully signed in!!",
-  });
+  try {
+    const hashedPassword = await bcrypt.hash(password, 5);
+
+    await UserModel.create({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+  } catch (e) {
+    res.json({
+      message: "User already exists",
+    });
+    errorThrown = true;
+  }
+
+  if (!errorThrown) {
+    res.json({
+      message: "You successfully signed up!!",
+    });
+  }
 });
 app.post("/signin", async function (req, res) {
   const email = req.body.email;
   const password = req.body.password;
 
-  const user = await UserModel.findOne({
+  const response = await UserModel.findOne({
     email: email,
-    password: password,
   });
+  if (!response) {
+    res.json({
+      message: "Invalid Credentials",
+    });
+  }
 
-  if (user) {
+  const matchedPassword = await bcrypt.compare(password, response.password);
+
+  if (matchedPassword) {
     const token = jwt.sign(
       {
-        id: user._id.toString(),
+        id: response._id.toString(),
       },
       JWT_SECRET
     );
